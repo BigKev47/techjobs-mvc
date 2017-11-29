a7835321c4b436a49d05352895d967ce7f5f92e3
Kevin Corlett <mediumkev@gmail.com>
2017-11-29 14:48:53 -0600

    Getting it to basically show me results, still not querying.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -21,6 +21,31 @@ public class SearchController {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
+ /*   @RequestMapping(value = "results")
+    public String listColumnValue(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "list-jobs";
+        } else {
+            ArrayList<HashMap<String,String>> items = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", items.size() + " Results");
+            model.addAttribute("column", searchType);
+            model.addAttribute("items", items);
+            return "list-column";
+        }
+    }*/
+    @RequestMapping(value = "results")
+    public String listJobsByColumnAndValue(Model model,
+                                                          @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("title", jobs.size()+" Results Found");
+        model.addAttribute("position type", jobs.);
+
+        return "search";
+    }
 
     // TODO #1 - Create handler to process search request and display results
 
